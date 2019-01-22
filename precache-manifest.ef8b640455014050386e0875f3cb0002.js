766a84103170a2a52c0e85d22e46981e75208595
Jakub Antolak <poprostuantolak@gmail.com>
2019-01-22 14:52:59 +0100

    Updates

---
+++ precache-manifest.ef8b640455014050386e0875f3cb0002.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "76c9c9aec2e9185ee6aa",
+    "url": "/svg-gradient-editor/static/js/runtime~main.76c9c9ae.js"
+  },
+  {
+    "revision": "5c12f231902c809727b5",
+    "url": "/svg-gradient-editor/static/js/main.5c12f231.chunk.js"
+  },
+  {
+    "revision": "f3605b14c59c938ae2c7",
+    "url": "/svg-gradient-editor/static/js/1.f3605b14.chunk.js"
+  },
+  {
+    "revision": "7d2d77da6368519f4d031826af93efff",
+    "url": "/svg-gradient-editor/index.html"
+  }
+];
